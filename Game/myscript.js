b92c80665ebc94db9f9e18cac3a41c7202a5aa21
Xiaoying-Huang <hxybecomexs@gmail.com>
2022-04-19 20:01:56 +0100

    Added Game

--- Game/myscript.js
+++ Game/myscript.js
@@ -5,21 +5,60 @@ let playerPaper=document.querySelector("#player-paper");
 let pcRock=document.querySelector("#pc-rock");
 let pcScissors=document.querySelector("#pc-scissors");
 let pcPaper=document.querySelector("pc-paper");
+let pcChoice=[pcRock, pcScissors, pcPaper];
+let youWin=docuemnt.querySelector("#you-win");
+let youLose=docuemnt.querySelector("#you-lose");
+let tieResult=document.querySelector("#tie");
 
-// Display the element chosen by player, and make those not selected disappear from the page;
+// PC choose Rock, Scissors or Paper randomly
+function pcSelect(arr){
+  // produce random seed integer from 0-2
+  let randomSeed=Math.floor(Math.random()*3); 
+  let pcSelected=arr[randomSeed];
+  // change the display of the chosen element from "none" to "block"
+  pcSelected.style.display="block";
+  }
+
+// Compare Player vs PC
+function gameOutcome(player,PC){
+  if(player==playerRock && PC==pcRock){
+    tieResult.style.display="block";
+  }else if(player==playerRock && PC==pcScissors){
+    youWin.style.display="block";
+  }else if(player==playerRock && PC==pcPaper){
+    youLose.style.display="block";
+  }else if(player==playerScissors && PC==pcRock){
+    youLose.style.display="block";
+  }else if(player==playerScissors && PC==pcScissors){
+    tieResult.style.display="block";
+  }else if(player==playerScissors && PC==pcPaper){
+    youWin.style.display="block";
+  }else if(player==playerPaper && PC==pcRock){
+    youWin.style.display="block";
+  }else if(player==playerPaper && PC==pcScissors){
+    youLose.style.display="block";
+  }else if(player==playerPaper && PC==pcPaper){
+    tieResult.style.display="block";
+  }else{
+    console.error();
+  }
+}
+
+  // Display the element chosen by player, and make those not selected disappear from the page;
 playerRock.addEventListener("click", ()=>{
   playerScissors.style.display="none";
   playerPaper.style.display="none";
+  pcSelect(pcChoice);
 })
 
 playerScissors.addEventListener("click", ()=>{
   playerRock.style.display="none";
   playerPaper.style.display="none";
+  pcSelect(pcChoice);
 })
 
 playerPaper.addEventListener("click", ()=>{
   playerRock.style.display="none";
   playerScissors.style.display="none";
+  pcSelect(pcChoice);
 })
-
-// PC choose Rock, Scissors or Paper randomly
